1244f3ae2185e1d50d9b33bf8e965d14010b6544
Nhan Hoang <nhan.hoang2@tiki.vn>
2022-08-01 23:10:25 +0700

    fix(scripts): only need 1 deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,55 @@
+const hre = require('hardhat');
+const chalk = require('chalk');
+const endpoints = require('../constants/layerzeroEndpoints.json');
+
+async function main() {
+  const chainId = hre.network.config.chainId || 31337; // Fallback to localhost
+  const lzEndpointAddr = endpoints[hre.network.name];
+  if (!lzEndpointAddr) {
+    throw new Error('No LZ endpoint for this network');
+  }
+
+  // First, deploy the bridge contract on dst
+  const Bridge = await hre.ethers.getContractFactory('Bridge');
+  const bridgeInstance = await Bridge.deploy(
+    chainId, // Network
+    lzEndpointAddr // lzEndpoint address
+  );
+
+  await bridgeInstance.deployed();
+  console.log(
+    chalk.green('✓'),
+    `Bridge contract deployed at ${chalk.blue(bridgeInstance.address)}`
+  );
+
+  // Then deploy a ERC20Handler on this network
+  const ERC20Handler = await hre.ethers.getContractFactory('ERC20Handler');
+  const erc20Handler = await ERC20Handler.deploy(
+    bridgeInstance.address // Bridge address
+  );
+  await erc20Handler.deployed();
+  console.log(
+    chalk.green('✓'),
+    `ERC20Handler contract deployed at ${chalk.blue(erc20Handler.address)}`
+  );
+
+  // After done deploying, show suggestions
+  console.log(
+    chalk.red('!'),
+    `Please config the resource with following commands:
+${chalk.gray(
+    '$ npx hardhat registerResource --bridge <address> --handler <address> --targetContract <address> --resourceId <address>'
+  )}
+${chalk.gray(
+    '$ npx hardhat setBurnable --bridge <address> --handler <address> --tokenContract <address>'
+  )}
+`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
