e1113979f3f1054cef692dc1a41ccc12571881d5
Nhan Hoang <nhan.hoang2@tiki.vn>
2022-08-02 11:14:18 +0700

    fix(contracts): enhance AsERC20 with decimals

--- contracts/token/AsERC20.sol
+++ contracts/token/AsERC20.sol
@@ -2,7 +2,32 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-// this is a MOCK for destination Token
+// this is a Astra wrapped ERC20 Token
 contract AsERC20 is ERC20PresetMinterPauser {
-    constructor(string memory name_, string memory symbol_) ERC20PresetMinterPauser(name_, symbol_) {}
+    uint8 private _decimals;
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_
+    ) ERC20PresetMinterPauser(name_, symbol_) {
+        _decimals = decimals_;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless this function is
+     * overridden;
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
 }
